df7f66087390d41f3f2f180f77db427adcfb3a63
r_svv <svv@renue.ru>
2022-11-19 14:43:50 +0500

    add java app

---
+++ src/main/java/com/example/testappmy/TestAppMyApplication.java
@@ -0,0 +1,13 @@
+package com.example.testappmy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestAppMyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestAppMyApplication.class, args);
+    }
+
+}
